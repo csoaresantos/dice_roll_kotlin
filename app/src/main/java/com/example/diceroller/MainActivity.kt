72ed4c79fcd388668358a00184d953638b5c03df
Charles S <charlessoaresdossantos@gmail.com>
2020-09-23 22:00:40 -0300

    added lateinit accessor method to avoid checknull a long of the application.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,13 +20,20 @@ class MainActivity : AppCompatActivity() {
 
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val result: TextView = findViewById(R.id.result_text)
+        val drawableResource = when(java.util.Random().nextInt(6)+1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
-        val resultInt = java.util.Random().nextInt(6)+1
-
-        result.text = resultInt.toString()
+        diceImage.setImageResource(drawableResource)
     }
 }
